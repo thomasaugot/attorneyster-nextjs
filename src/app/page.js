d0a1576531b8d33b09f3cca9f3fdf80de0f548cf
thomasaugot <thomas.augot@hotmail.fr>
2024-03-08 15:18:25 +0100

    footer ok

--- src/app/page.js
+++ src/app/page.js
@@ -45,10 +45,10 @@ import johnDavid from "@/assets/img/homepage/john.svg";
 import thomasDanyel from "@/assets/img/homepage/thomas.svg";
 import nikolona from "@/assets/img/homepage/nokolona.svg";
 import michalDavid from "@/assets/img/homepage/michal.svg";
-import instagram from "@/assets/img/homepage/instagram.png";
-import facebook from "@/assets/img/homepage/facebook.png";
-import twitter from "@/assets/img/homepage/Twiter.png";
-import pinterest from "@/assets/img/homepage/Pintrest.png";
+import instagram from "@/assets/img/homepage/instagram.svg";
+import facebook from "@/assets/img/homepage/facebook.svg";
+import twitter from "@/assets/img/homepage/twitter.svg";
+import pinterest from "@/assets/img/homepage/pinterest.svg";
 import call from "@/assets/img/homepage/Call.png";
 import bgillu2 from "@/assets/img/homepage/bg-illu-2.svg";
 import writingHand from "@/assets/img/homepage/writing-hand.svg";
@@ -58,6 +58,10 @@ import lawyers from "@/assets/img/homepage/lawyers.svg";
 import courtBuilding from "@/assets/img/homepage/court-building.svg";
 import library from "@/assets/img/homepage/library.svg";
 import library2 from "@/assets/img/homepage/library-2.svg";
+import bgIllustration3 from "@/assets/img/homepage/bg-illu-3.svg";
+import logo from "@/assets/img/misc/logo-full.svg";
+import separatorLeft from "@/assets/img/homepage/separatorLeft.png";
+import separatorRight from "@/assets/img/homepage/separatorRight.png";
 
 export default function Home() {
   const expertiseAreas = [
@@ -185,6 +189,7 @@ export default function Home() {
 
   const [isExpertiseVisible, setIsExpertiseVisible] = useState(false);
   const [isPartnersVisible, setIsPartnersVisible] = useState(false);
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const { ref: expertiseRef, inView: expertiseInView } = useInView({
     threshold: 0.2,
@@ -193,6 +198,16 @@ export default function Home() {
   const { ref: partnersRef, inView: partnersInView } = useInView({
     threshold: 0.2,
   });
+
+  const { ref: formRef, inView: formInView } = useInView({
+    threshold: 0.2,
+  });
+
+  useEffect(() => {
+    if (formInView) {
+      setIsFormVisible(true);
+    }
+  }, [formInView]);
 
   useEffect(() => {
     if (expertiseInView) {
@@ -205,6 +220,14 @@ export default function Home() {
       setIsPartnersVisible(true);
     }
   }, [partnersInView]);
+
+  const handlePhoneClick = () => {
+    window.location.href = "tel:+011987654321";
+  };
+
+  const handleEmailClick = () => {
+    window.location.href = "mailto:thomas.augot@hotmail.fr";
+  };
 
   return (
     <main className="relative">
@@ -315,7 +338,7 @@ export default function Home() {
           </div>
           <div className="w-[0.5px] h-[105px] bg-white opacity-40"></div>
           <p className="w-[50%]">
-            There are{" "}
+            There are
             <span className="text-camel">many variations of passages</span> of
             Lorem Ipsum available, but the majority have su alteration in some
             form, by injected humour, oir
@@ -889,12 +912,15 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="h-[120vh] py-[15vh] px-[15vh] relative flex justify-center items-center">
+      <section
+        className="h-[120vh] py-[15vh] px-[15vh] relative flex justify-center items-center"
+        ref={formRef}
+      >
         <div
           className="absolute inset-0 flex justify-center items-center bg-cover bg-center z-0 overflow-hidden"
           style={{ backgroundImage: `url(${bgillu2.src})` }}
         >
-          <HomepageForm />
+          {isFormVisible && <HomepageForm />}
         </div>
       </section>
       <section className="bg-darkBlue px-[10vw] py-[15vh]">
@@ -962,8 +988,236 @@ export default function Home() {
             );
           })}
         </div>
-        <div></div>
       </section>
+      <section className="h-[40vh] relative flex justify-center items-center">
+        <div
+          className="absolute inset-0 flex justify-center items-center bg-cover bg-center z-0 overflow-hidden"
+          style={{ backgroundImage: `url(${bgIllustration3.src})` }}
+        ></div>
+      </section>
+      <footer className="bg-darkBlue px-[10vw] py-[15vh] flex flex-col">
+        <div className="flex gap-6 justify-center">
+          <Image
+            src={separatorLeft}
+            width={"auto"}
+            height={"auto"}
+            alt="separator"
+          />
+          <Image src={logo} width={250} height={79} alt="logo" />
+          <Image
+            src={separatorRight}
+            width={"auto"}
+            height={"auto"}
+            alt="separator"
+          />
+        </div>
+        <div className="flex flex-wrap justify-between my-24">
+          <div className="w-auto">
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <h1 className="text-2xl">Quick Links</h1>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <div className="flex gap-8">
+              <ul>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Contact
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Blog Posts
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Lawyers
+                  </Link>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Lawyer Single
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Case Results
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Practice Areas
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Packages
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/"} className="hover:underline">
+                    Package Single
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <h1 className="text-2xl">Utility Page</h1>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <ul>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Start Here
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Style Guide
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  404 Not Found
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Password Protected
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Licenses
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Changelog
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <h1 className="text-2xl">Practice Areas</h1>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <ul>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Family Law
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Criminal Law
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Personal Injury
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Real Estate Law
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="hover:underline">
+                  Business Law
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <h1 className="text-2xl">Contact Us</h1>
+            <div className="w-full h-[1px] bg-white/25 my-4"></div>
+            <ul>
+              <li>Head Office Address 121 King Street,</li>
+              <li>Melbourne West,Australia</li>
+              <br />
+              <li className="cursor-pointer" onClick={handlePhoneClick}>
+                Phone: (011) 9876 54321
+              </li>
+              <li className="cursor-pointer" onClick={handleEmailClick}>
+                Email: info@example.com
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-full h-[1px] bg-white/25 mb-6"></div>
+        <div className="flex justify-between">
+          <p>
+            Copyright © <span className="text-camel">Attorney Law</span> |
+            Designed by <span className="text-camel">VictorFlow</span> Templates
+            - Coded by{" "}
+            <Link href={"https://thomasaugot.com/"} target="blank">
+              <span className="text-camel cursor-pointer hover:underline">
+                Thomas Augot
+              </span>
+            </Link>
+          </p>
+          <div className="flex flex justify-center gap-6 px-4">
+            <p className="text-camel">Follow : </p>
+            <Link href={"/"}>
+              <Image
+                src={instagram}
+                width={"auto"}
+                height={"auto"}
+                alt="portait"
+                className="fill-white	"
+              />
+            </Link>
+            <Link href={"/"}>
+              <Image
+                src={facebook}
+                width={"auto"}
+                height={"auto"}
+                alt="portait"
+                className="fill-white	"
+              />
+            </Link>
+            <Link href={"/"}>
+              <Image
+                src={twitter}
+                width={"auto"}
+                height={"auto"}
+                alt="portait"
+                className="fill-white	"
+              />
+            </Link>
+            <Link href={"/"}>
+              <Image
+                src={pinterest}
+                width={"auto"}
+                height={"auto"}
+                alt="portait"
+                className="fill-white	"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="w-full h-[1px] bg-white/25 mt-6"></div>
+      </footer>
     </main>
   );
 }
